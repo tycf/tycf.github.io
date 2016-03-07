7de132864038c4db8bd9931548b787abaad629cc
zhucbeta <zhucbeta@gmail.com>
2016-03-07 16:37:21 +0800

    clear

--- files/______-_0303_-todo/data.js
+++
@@ -1,7 +0,0 @@
-﻿$axure.loadCurrentPage(
-(function() {
-    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
-    var _creator = function() { return _(b,c,d,e,f,g,h,[i],j,_(k,l,m,n,o,p,q,_(r,s,t,u,v,_(w,x,y,z),A,null,B,u,C,u,D,E,F,null,G,H,I,J,K,L,M,H),N,_(),O,_(),P,_(Q,[])),R,_(),S,_());}; 
-var b="url",c="______-_0303_-todo.html",d="generationDate",e=new Date(1457054797253.77),f="isCanvasEnabled",g=false,h="variables",i="OnLoadVariable",j="page",k="packageId",l="93f16f2d6a3f4b6ca010f3ee48faf2a6",m="type",n="Axure:Page",o="name",p="对话框切换 - 0303 -todo",q="style",r="baseStyle",s="627587b6038d43cca051c114ac41ad32",t="pageAlignment",u="near",v="fill",w="fillType",x="solid",y="color",z=0xFFFFFFFF,A="image",B="imageHorizontalAlignment",C="imageVerticalAlignment",D="imageRepeat",E="auto",F="favicon",G="sketchFactor",H="0",I="colorStyle",J="appliedColor",K="fontName",L="Applied Font",M="borderWidth",N="adaptiveStyles",O="interactionMap",P="diagram",Q="objects",R="masters",S="objectPaths";
-return _creator();
-})());
